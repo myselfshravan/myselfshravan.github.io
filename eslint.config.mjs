94cd3ccdc171de541c0809f97b27a7df7df806fc
myselfshravan <shravanrevanna@gmail.com>
2025-08-09 00:11:30 +0530

    update ESLint configuration to ignore output and public directories

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ['out/**', 'public/**'],
+  },
   ...compat.extends(
     'next/core-web-vitals',
     'next/typescript',
