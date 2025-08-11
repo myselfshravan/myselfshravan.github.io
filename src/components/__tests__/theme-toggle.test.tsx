73251d8a2eb396048eac6bc22c547e5d41e8674d
myselfshravan <shravanrevanna@gmail.com>
2025-08-11 17:10:39 +0530

    chore: update .gitignore to include coverage directory and enhance theme-toggle test with jest-dom

--- src/components/__tests__/theme-toggle.test.tsx
+++ src/components/__tests__/theme-toggle.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import { ThemeToggle } from '../theme-toggle';
 import { ThemeProvider } from '../theme-provider';
 
